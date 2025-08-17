9bb79a7d17bbee14431581a495fc80e3661d4281
Trent Johnson <trentj@netflixcontractors.com>
2025-08-17 11:40:00 -0700

    anoter attempt at file upload increase

--- index.cjs
+++ index.cjs
@@ -17,6 +17,7 @@ const API_ROOT = '/api';
 
 app.use(cors({ origin: CORS_ORIGINS.split(',') }));
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(favicon('./favicon.ico'));
 
 app.get('/', (req, res) => {
